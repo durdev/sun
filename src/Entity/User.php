dbe61c81d2fcac3a1dfe706a4c02984193bc4fa3
Davi Uchoa Rodrigues <daviuchoa38@gmail.com>
2021-05-30 22:23:09 -0300

    adds the name field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -37,6 +37,13 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\GreaterThan(3)
+     */
+    private $name;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -116,5 +123,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
